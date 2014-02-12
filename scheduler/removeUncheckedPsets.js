fcd4d50e8a2d1504ff755945306228272ff6b617
Andy Jiang <andyjiang@gmail.com>
2014-02-11 20:01:57 -0800

    updated removeuncheckedpsets script

--- scheduler/removeUncheckedPsets.js
+++ scheduler/removeUncheckedPsets.js
@@ -1,23 +1,15 @@
-require('./app/models/pset');
+require('../app/models/pset');
 
 var mongoose = require('mongoose')
-  , database = require('./config/database')
+  , database = require('../config/database')
   , Pset = mongoose.model('Pset')
   , moment = require('moment');
 
 mongoose.connect(database.url);
 
-// Go through the model
 function removeUncheckedPsets () {
-  Pset.find({}, function (err, psets) {
-    for (var i = 0; i < psets.length; i++) {
-      var monthsFromNow = moment().diff(moment(psets[i].last_checked_on),
-        'months');
-
-      if (monthsFromNow > 0)
-        psets[i].remove();
-    }
-  });
+  var oneMonthAgo = moment().subtract('months', 1).toDate();
+  Pset.find({last_checked_on: {$lt: oneMonthAgo}}).remove();
 }
 
 removeUncheckedPsets();
